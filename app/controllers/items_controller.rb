1e199b26dab44a08d450c0a4054dae164c3dc30f
GregT <taylor.gregory2@gmail.com>
2022-08-06 17:13:27 -0600

    commit tree 2727e8157060050a533b4dc846ce937a296731d3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+
+    def index
+        render json: Item.all, include: :user
+    end
+
+    def show
+        render json: Item.find_by(id:params[:id]), include: :user
+    end
+    
 end
